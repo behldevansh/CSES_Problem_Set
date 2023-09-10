66bfb58b2688fb0fabffd71bea8d4a9422492fe5
behldevansh <heydevanshbehl@gmail.com>
2023-09-10 13:08:23 +0530

    added movie festival soln

---
+++ Movie_Festival.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+
+    ll T=1;
+    while(T--){
+       vector<pair<ll,ll>> v;
+       ll n;
+       cin>>n;
+       for(ll i=0;i<n;i++){
+          ll a,c;
+          cin>>a>>c;
+          v.push_back({c,a});
+       }
+       sort(v.begin(),v.end());
+       ll curr=v[0].first;
+       ll ans=1;
+       for(ll i=1;i<n;i++){
+           if(v[i].second>=curr){
+            curr=v[i].first;
+            ans++;
+           }
+       }
+       cout<<ans<<"\n";
+    }
+return 0;
+}
